b1e0660cdfbcf4d465724bada82d63ee5e0805b1
Connor Dickey <connor@kd9lsv.me>
2022-11-03 21:28:24 -0500

    Reformatted webstie to pages. Allow for Maps to read geojson. Started to write SQL Commands for dataframes and individual paylaods.

---
+++ pages/2_Main_Score.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import json, socket, time, threading
+import geopandas as gpd
+import pydeck as pdk
+
+# import mysql.connector
+st.set_page_config(layout="wide")
+    
+# def init_connection():
+#     return mysql.connector.connect(**st.secrets["mysql"])
+
+# def run_query(query):
+#     with conn.cursor() as cur:
+#         cur.execute(query)
+#         return cur.fetchall()
+
+# Writing and porting data
+@st.experimental_memo
+def combined_dashboard(callsigns,tab):
+    st.write("IN-Progress")
+  #  command_master="Select Contest_Call, points*mults AS total_score,count(Q) as QSOs, sum(points) as Points, sum(mults) as Mults FROM qsos"
+  #  score_frame=pd.read_sql(command_master,,)
+    # Summed QSO Activity by Band
+    # Table - Score, Callsign, Place, Active, Radio 1 Data
+    # Map of active contesters - Home Lat. Long
+
+    combined_table= pd.DataFrame(
+        columns=["Active","Callsign","Score","Place","Radio1"]
+    )
+    with tab:
+        st.dataframe(combined_table)
---
+++ pages/main_board.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# import mysql.connector
+st.set_page_config(layout="wide")
+st.title('Contest Master Dashboard')
+'''
+Welcome to the Contest Master Dashboard. This is a work-in-progress. Any bug issues will be appreciated to head to dashboardissues@kd9lsv.me or update via Github. 
+'''
