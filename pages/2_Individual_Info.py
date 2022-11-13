d8c18b3bd742147b10cef192b4bd8f46bd7d454a
Connor Dickey <connor@kd9lsv.me>
2022-11-13 10:10:18 -0600

    Refresh test and add exceptions to not receiving data.

--- pages/2_Individual_Info.py
+++ pages/2_Individual_Info.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 import dashboard_default as dash
 import leafmap.foliumap as leafmap
+st.set_page_config(layout="wide")
+
 def qso_line_map(_m, con, callsign, group, time):
     base_command = "SELECT q.mycall as 'Own Call', h.lat AS 'latfrom', h.lon as 'lonfrom',q.external_Call as 'Contact Call',CONVERT(q.lat,FLOAT) as 'latto', CONVERT(q.lon,FLOAT) as 'lonto',TIMESTAMPDIFF(MINUTE,NOW(),`timestamp`) as 'Time Elapsed', `timestamp` FROM qsos as q INNER JOIN home as h ON h.Contest_Call=q.mycall WHERE ABS(TIMESTAMPDIFF(MINUTE,NOW(),`timestamp`)) <="+str(time)
     # allow all QSOS or only one callsign
@@ -18,83 +20,104 @@ def qso_line_map(_m, con, callsign, group, time):
         'Own Call','Contact Call'
     ],geo_type='LINESTRING')
     return spokemap
-def app():
-    st.set_page_config(layout="wide")
-    #callsigns = ["K9CT","KD9LSV","AA0Z","N0AX","W0ECC","KD9WHO"]
-    callsigns = dash.run_query(dash.connect(),"SELECT DISTINCT Contest_Call FROM home ORDER BY Contest_Call ASC;")
-    with st.sidebar:
-        chosen_callsign=st.selectbox('Choose Callsign',callsigns)
-        time_value=st.slider("Map Time Max",min_value=10, max_value=60,value=30, step=1)
+@st.experimental_memo(ttl=15)
+def app(chosen_callsign,time_value):
     with open("frontend/css/streamlit.css") as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     # Payload SQL Commands
-    st.write(dash.run_query(dash.connect(),"SELECT * from qsos limit 1;"))
-    command_run="select external_call as Last Run QSO, SEC_TO_TIME(timestampdiff(SECOND,`contest`.`qsos`.`timestamp`,current_timestamp())) as Time from qsos as q where q.mycall ='"+chosen_callsign+"' and IsRunQSO=1 ORDER BY timestamp DESC LIMIT 1;"
-    command_sp="select external_call as 'Last S&P QSO', SEC_TO_TIME(timestampdiff(SECOND,`contest`.`qsos`.`timestamp`,current_timestamp())) as Time from qsos as q where q.mycall ='"+chosen_callsign+"' and IsRunQSO=0 ORDER BY timestamp DESC LIMIT 1;"
-    command_run= "SELECT * from last_qsos where `Contest Call` ='"+chosen_callsign+"';"
+    command_run="(select external_call as 'Last Run QSO', SEC_TO_TIME(timestampdiff(SECOND,`timestamp`,current_timestamp())) as Time from qsos as q where q.mycall ='"+chosen_callsign+"' and IsRunQSO=1 ORDER BY timestamp DESC LIMIT 1)"
+    #st.write(dash.run_query(dash.connect(),"SELECT * from qsos limit 1;"))
+    command_sp="(select external_call as 'Last Run QSO', SEC_TO_TIME(timestampdiff(SECOND,`timestamp`,current_timestamp())) as Time from qsos as q where q.mycall ='"+chosen_callsign+"' and IsRunQSO=0 ORDER BY timestamp DESC LIMIT 1);"
     command_calc="Select JSON_ONJECT(c.Top_OP_Mults, c.Top_OP_Score FROM calculated AS c WHERE c.Contest_Call="+chosen_callsign+"ORDER BY c.timestamp DESC LIMIT 1;"
-    command_radio1="SELECT r.OpCall, r.IsRunning, r.Freq, r.FunctionKeyCaption as Macro, r.IsTransmitting, r.FocusRadioNr from radio AS r WHERE r.station_call='"+chosen_callsign+"' and r.RadioNr=1 ORDER BY r.timestamp DESC Limit 1;" 
-    command_radio2="SELECT r.OpCall, r.IsRunning, r.Freq, r.FunctionKeyCaption as Macro, r.IsTransmitting, r.FocusRadioNr from radio AS r WHERE r.station_call='"+chosen_callsign+"' and r.RadioNr=2 ORDER BY r.timestamp DESC Limit 1;"
+    command_radio1="SELECT r.OpCall, r.IsRunning, r.Freq/100 as Freq, r.FunctionKeyCaption as Macro, r.IsTransmitting, r.FocusRadioNr from radio AS r WHERE r.station_call='"+chosen_callsign+"' and r.RadioNr=1 ORDER BY r.timestamp DESC Limit 1;" 
+    command_radio2="SELECT r.OpCall, r.IsRunning, r.Freq/100 as Freq, r.FunctionKeyCaption as Macro, r.IsTransmitting, r.FocusRadioNr from radio AS r WHERE r.station_call='"+chosen_callsign+"' and r.RadioNr=2 ORDER BY r.timestamp DESC Limit 1;"
     command_score="SELECT (s.totalpoints * s.totalmults) as 'Total Score', s.totalnoqsos as QSOs, s.totalpoints as Points, s.totalmults as Mults, s.total_rate FROM score AS s WHERE s.station_call='"+chosen_callsign+"' ORDER BY s.timestamp DESC LIMIT 1;"
     command_q="SELECT q.external_call, q.band, q.mode, q.zone as exchange, q.distance, q.operator FROM qsos as q where q.`mycall`='"+chosen_callsign+"' ORDER BY q.timestamp DESC LIMIT 1;"
+    command_dash=command_run+" UNION "+command_sp
     connection=dash.connect()
+    dash_qsos=dash.run_query(connection,command_dash)
     last_qso=dash.run_query(connection,command_q).to_dict()
     value_radio1=dash.run_query(connection,command_radio1).to_dict()
     value_radio2=dash.run_query(connection,command_radio2).to_dict()
     score_update=dash.run_query(connection,command_score).to_dict()
     # Sidebar Configuration
-  
     # Payload Defaults
     payload_test= {"Last_Run_QSO_Time":"00:20:00","Last_Run_QSO":"KD9LSV","Last_SP_QSO_Time":"00:05:02","Last_SP_QSO":"WT2P","Top_OP_Mults":"K9CT","Radio_1_Operator":"AA0Z","Radio_1_Mode":"Run","Radio_1_Freq":"21245.4","Radio_1_Status":"","Radio_1_Macro":"","Radio_1_TX":"","Radio_1_Focus":"","Radio_1_Rate":250,"Radio_2_Operator":"AB9YC","Radio_2_Mode":"S&P","Radio_2_Freq":"21295.7","Radio_2_Status":"","Radio_2_Macro":"","Radio_2_TX":"","Radio_2_Focus":"","Radio_2_Rate":60,"Total_Score":2534136,"Total_QSOs":2245,"Total_Points":1914,"Total_Mults":1324,"Current_Total_Rate":125,"Top_OP_Score":"AB9YC","Last_QSO":"K1AR","Last_QSO_Band":"20M","Last_QSO_Mode":"PH","Last_QSO_Exchange":"5","Last_QSO_Distance":"1503 Mi","Last_QSO_OP":"AB9YC"}
     payload=payload_test
+    payload["dashboard_qso"]=dash_qsos
     payload["last_qso"]=last_qso
     payload["score"]=score_update
     payload["radio1"]=value_radio1
     payload["radio2"]=value_radio2
     # payload["last_time"]=
     ### End_Defaults
+    placeholder=st.empty()
+    placeholder2=st.empty()
+    placeholder3=st.empty()
+    with placeholder.container():
+        #payload=select_individual_data(chosen_callsign)
+        col_LastQSO, col_CurrentQSO, col_Score = st.columns([.9,5,1])
+        # Last QSO Info
+        with col_LastQSO:
+            st.markdown("## Last QSO's")
+            try:
+                st.metric("Last Run QSO",payload["dashboard_qso"]["Last Run QSO"][0])
+            except (IndexError):
+                st.metric("Last Run QSO","-")
+            try:
+                st.metric("Last Run QSO",str(payload["dashboard_qso"]["Time"][0])[-8:])
+            except (IndexError):
+                st.metric("Last Run QSO","-")
+            try:
+                st.metric("Last S&P QSO",payload["dashboard_qso"]["Last Run QSO"][1])
+            except (IndexError):
+                st.metric("Last Run QSO","-")
+            try:
+                st.metric("Last Run QSO",str(payload["dashboard_qso"]["Time"][1])[-8:])
+            except (IndexError):
+                st.metric("Last Run QSO","-")
+            try:
+                st.metric("Top OP Mults",payload["Top_OP_Mults"])  
+            except (IndexError):
+                st.metric("Last Run QSO","-")
+        # QSO Map
+        with col_CurrentQSO:
+            st.markdown("<h2><center> QSO Map </center></h2>",unsafe_allow_html=True) 
+            base_map = leafmap.Map(draw_export=True)
+            base_layer = dash.geo_map(base_map)
+            qso_line_layer = qso_line_map(base_map,dash.connect(),chosen_callsign,"Y",time_value) 
+            #st.write(qso_line_layer)
+            base_map.add_gdf(base_layer, layer_name="ARRL Sections")
+            try:
+                base_map.add_gdf(qso_line_layer, layer_name="QSOs")
+            except IndexError:
+                with st.sidebar:
+                    st.write("No QSOs in last "+str(time_value)+" minutes.")
+            except :
+                with st.sidebar:
+                    st.write("Error on QSO Layer")
+            base_map.set_center(-97,38,3.5)
+            base_map.to_streamlit(width=900, height=550)
+            backup_map = leafmap.Map(draw_export=True)
+        # Score Column
+        with col_Score:
 
-    #payload=select_individual_data(chosen_callsign)
-
-    col_LastQSO, col_CurrentQSO, col_Score = st.columns([.9,5,1])
-    # Last QSO Info
-    with col_LastQSO:
-        st.markdown("## Last QSO's")
-        st.metric("Last Run QSO",payload["Last_Run_QSO_Time"])
-        st.metric("Last Run QSO",payload["Last_Run_QSO"])
-        st.metric("Last S&P QSO",payload["Last_SP_QSO_Time"])
-        st.metric("Last Run QSO",payload["Last_SP_QSO"])
-        st.metric("Top OP Mults",payload["Top_OP_Mults"])   
-    # QSO Map
-    with col_CurrentQSO:
-        st.markdown("<h2><center> QSO Map </center></h2>",unsafe_allow_html=True) 
-        base_map = leafmap.Map(draw_export=True)
-        base_layer = dash.geo_map(base_map)
-        qso_line_layer = qso_line_map(base_map,dash.connect(),chosen_callsign,"Y",time_value) 
-        #st.write(qso_line_layer)
-        base_map.add_gdf(base_layer, layer_name="ARRL Sections")
-        try:
-            base_map.add_gdf(qso_line_layer, layer_name="QSOs")
-        except IndexError:
-            with st.sidebar:
-               st.write("No QSOs in last "+str(time_value)+" minutes.")
-        except:
-            with st.sidebar:
-                st.write("Error on QSO Layer")
-        base_map.set_center(-97,38,3.5)
-        base_map.to_streamlit(width=900, height=550)
-        backup_map = leafmap.Map(draw_export=True)
-    # Score Column
-    with col_Score:
-        st.markdown("## Score")
-        st.metric("Total Score",payload["Total_Score"])
-        st.metric("QSO's",payload["Total_QSOs"])
-        st.metric("Points",payload["Total_Points"])
-        st.metric("Mults",payload["Total_Mults"])
-        st.metric("Current Total QSO Rate",payload["Current_Total_Rate"])
-        st.metric("Top OP Score",payload["Top_OP_Score"]) 
+            st.markdown("## Score")
+            try:
+                st.metric("Total Score",payload["score"]["Total Score"][0])
+                st.metric("QSO's",payload["score"]["QSOs"][0])
+                st.metric("Points",payload["score"]["Points"][0])
+                st.metric("Mults",payload["score"]["Mults"][0])
+                st.metric("Current Total QSO Rate",payload["score"]["total_rate"][0])
+                st.metric("Top OP Score",payload["Top_OP_Score"]) 
+            except (IndexError, KeyError) as error:
+                st.metric("Total Score","-")
+                st.metric("QSO's","-")
+                st.metric("Points","-")
+                st.metric("Mults","-")
+                st.metric("Current Total QSO Rate","-")
     # Latest QSO
-    with st.container():
+    with placeholder2.container():
         col_space_a, col_Last, col_Last_QSO,col_band,col_mode,col_exchange,col_distance,col_op, col_Score_b = st.columns([1.0,1.3,.7,.7,.7,.7,.7,.7,1.5])
         with col_Last:
             st.markdown("## Latest QSO:")
@@ -118,7 +141,7 @@ def app():
                 st.write("Missing QSOS")
 
     # Radio Data
-    with st.container():
+    with placeholder3.container():
         col_1_title, col_2_title= st.columns([1,1])
         with col_1_title:
             st.title("Radio 1 Data")
@@ -136,7 +159,7 @@ def app():
             with col_4:
                 st.metric("Status",payload["radio1"]["IsTransmitting"][0])
             with col_5:
-                if (payload["radio1"]["FunctionKeyCaption"][0]==''):
+                if (payload["radio1"]["Macro"][0]==''):
                     st.metric("Macro","Hello")
                 else:
                     st.metric("Macro",payload["radio1"]["FunctionKeyCaption"][0])
@@ -144,8 +167,8 @@ def app():
                 st.metric("Radio 1 Rate",payload["radio1"]["FocusRadioNr"][0])
             with col_7:
                 st.markdown("TX and Focus")
-        except:
-            with col_4:
+        except KeyError as e:
+            with col_3:
                 st.write("No Radio1 Data")
             # Radio 2
         try:
@@ -164,7 +187,12 @@ def app():
             with col_7_2:
                 st.markdown("TX and Focus")
         except:
-            with col_4_2:
-                st.write("Missing Radio 2 Data")
+            with col_3_2:
+                st.write("No Radio 2 Data")
 
-app()
+#callsigns = ["K9CT","KD9LSV","AA0Z","N0AX","W0ECC","KD9WHO"]
+callsigns = dash.run_query(dash.connect(),"SELECT DISTINCT Contest_Call FROM home ORDER BY Contest_Call ASC;")
+with st.sidebar:
+    chosen_callsign=st.selectbox('Choose Callsign',callsigns)
+    time_value=st.slider("Map Time Max",min_value=10, max_value=60,value=30, step=1)
+app(chosen_callsign,time_value)
